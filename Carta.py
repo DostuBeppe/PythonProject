5afda1f2513fd6a8c604ea67db8adea88b220202
Giuseppe Dostuni <giuseppe.dostuni@edu.unito.com>
2019-04-16 12:15:55 +0200

    create cards

---
+++ Carta.py
@@ -0,0 +1,82 @@
+class Carta(object):
+
+    def __init__(self, nome_seme, valore):
+        self.nome_seme = nome_seme
+        self.valore = valore
+
+    def getVal(self):
+        return self.valore
+
+    def toString(self):
+        return "| " + self.nome_seme + " |"
+
+
+assoC = Carta("Asso di Cuori", 1)
+assoF = Carta("Asso di Fiori", 1)
+assoQ = Carta("Asso di Quadri", 1)
+assoP = Carta("Asso di Picche", 1)
+
+dueC = Carta("Due di Cuori", 2)
+dueF = Carta("Due di Fiori", 2)
+dueQ = Carta("Due di Quadri", 2)
+dueP = Carta("Due di Picche", 2)
+
+treC = Carta("Tre di Cuori", 3)
+treF = Carta("Tre di Fiori", 3)
+treQ = Carta("Tre di Quadri", 3)
+treP = Carta("Tre di Picche", 3)
+
+quattroC = Carta("Quattro di Cuori", 4)
+quattroF = Carta("Quattro di Fiori", 4)
+quattroQ = Carta("Quattro di Quadri", 4)
+quattroP = Carta("Quattro di Picche", 4)
+
+cinqueC = Carta("Cinque di Cuori", 5)
+cinqueF = Carta("Cinque di Fiori", 5)
+cinqueQ = Carta("Cinque di Quadri", 5)
+cinqueP = Carta("Cinque di Picche", 5)
+
+seiC = Carta("Sei di Cuori", 6)
+seiF = Carta("Sei di Fiori", 6)
+seiQ = Carta("Sei di Quadri", 6)
+seiP = Carta("Sei di Picche", 6)
+
+setteC = Carta("Sette di Cuori", 7)
+setteF = Carta("Sette di Fiori", 7)
+setteQ = Carta("Sette di Quadri", 7)
+setteP = Carta("Sette di Picche", 7)
+
+ottoC = Carta("Otto di Cuori", 8)
+ottoF = Carta("Otto di Fiori", 8)
+ottoQ = Carta("Otto di Quadri", 8)
+ottoP = Carta("Otto di Picche", 8)
+
+noveC = Carta("Nove di Cuori", 9)
+noveF = Carta("Nove di Fiori", 9)
+noveQ = Carta("Nove di Quadri", 9)
+noveP = Carta("Nove di Picche", 9)
+
+dieciC = Carta("Dieci di Cuori", 10)
+dieciF = Carta("Dieci di Fiori", 10)
+dieciQ = Carta("Dieci di Quadri", 10)
+dieciP = Carta("Dieci di Picche", 10)
+
+jackC = Carta("Jack di Cuori", 10)
+jackF = Carta("Jack di Fiori", 10)
+jackQ = Carta("Jack di Quadri", 10)
+jackP = Carta("Jack di Picche", 10)
+
+donnaC = Carta("Donna di Cuori", 10)
+donnaF = Carta("Donna di Fiori", 10)
+donnaQ = Carta("Donna di Quadri", 10)
+donnaP = Carta("Donna di Picche", 10)
+
+reC = Carta("Re di Cuori", 10)
+reF = Carta("Re di Fiori", 10)
+reQ = Carta("Re di Quadri", 10)
+reP = Carta("Re di Picche", 10)
+
+# assoUC = ("Asso di Cuori", 11)
+# assoUF = ("Asso di Fiori", 11)
+# assoUQ = ("Asso di Quadri", 11)
+# assoUP = ("Asso di Picche", 11)
